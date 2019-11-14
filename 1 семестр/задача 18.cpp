0c79e8efdf1215cfad529274d33142af10acccbf
undomiel17 <55258395+undomiel17@users.noreply.github.com>
2019-11-15 00:39:10 +0300

    Add files via upload

---
+++ 1 семестр/задача 18.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+
+   long int qsort(long int* a, int left,int right,int k) {
+     long int x = a[(left+right) / 2];
+     int y=left,z=right;
+     while(y<=z) {
+         while(a[y] < x) y++;
+         while(a[z] > x) z--;
+         if(y<=z) {
+             swap(a[y],a[z]);
+ y++; z--;
+         }
+     }
+     if(left<=k && k<=z)
+     return qsort(a,left,z,k);
+     if( y<=k && k<=right)
+     return qsort(a,y,right,k);
+     return a[k];
+}
+
+int main()
+{
+    int n, left, right, k;
+    cin >>n>> left >> right >> k;
+    long int *a= new long int[n];
+        for (int i = 0; i < n; i++) {
+ cin >> a[i];
+        }
+        cout << qsort (a,left-1, right-1, k-1); // учитывается, что пользователь не знает, что массив задаётся с нулевого индекса
+        delete [] a;
+    return 0;
+}
