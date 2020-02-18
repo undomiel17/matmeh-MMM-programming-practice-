bb002212a5c4058b6d4a1057286f72473e5553ae
undomiel17 <55258395+undomiel17@users.noreply.github.com>
2020-02-19 02:16:22 +0300

    Update and rename 2 семестр/2020.02.11/скобки.cpp to 2 семестр/семинар/2020.02.11/скобки.cpp

--- 2 семестр/семинар/2020.02.11/скобки.cpp
+++ 2 семестр/семинар/2020.02.11/скобки.cpp
@@ -9,24 +9,24 @@ int main()
 stack <char> sk;
 string a;
 cin>>a;
-if ((a[0]=='(')||(a[0]=='[')) sk.push(a[0]); else {
+if ((a[0]=='(')||(a[0]=='[')) sk.push(a[0]); else { //если начинается с закрывающейся
         cout<<"no";
         return 0;
         }
 for (int i=1; i< a.size(); i++){
     if ((a[i]=='(')||(a[i]=='[')) sk.push(a[i]);
 
-    else {if (sk.empty()){
+    else {if (sk.empty()){ //если закрывающихся больше
         cout<<"no";
         return 0;
         }
-        if ((!sk.empty())&&(abs(a[i]-sk.top())<3)) sk.pop(); else {
+        if ((!sk.empty())&&(abs(a[i]-sk.top())<3)) sk.pop(); else { //если не совпадают
         cout<<"no";
         return 0;
         }
     }
 }
-if (sk.empty()) cout<<"yes"; else cout<<"no";
+if (sk.empty()) cout<<"yes"; else cout<<"no"; //если в стеке ещё что-то осталось
 
     return 0;
 }
