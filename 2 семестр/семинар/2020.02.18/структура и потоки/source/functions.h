adf6cf718603c526b000665d034c5a7e8873af65
undomiel17 <55258395+undomiel17@users.noreply.github.com>
2020-04-02 20:38:58 +0300

    Update functions.h

--- 2 семестр/семинар/2020.02.18/структура и потоки/source/functions.h
+++ 2 семестр/семинар/2020.02.18/структура и потоки/source/functions.h
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
 struct Bio;
 void put_on_file (struct Bio man, const char* path);
-get_from_file (struct Bio man, const char* path)
+get_from_file (struct Bio man, const char* path);
