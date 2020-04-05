7d21648c2d895285f6ad18041d9665b280c4bf86
undomiel17 <55258395+undomiel17@users.noreply.github.com>
2020-04-05 18:56:33 +0300

    Add files via upload

---
+++ 2 семестр/семинар/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <cmath>
+#include <locale>
+using namespace std;
+
+
+    void swap (int *a, int i, int j){
+    int s=a[i];
+        a[i]=a[j];
+        a[j]=s;
+    }
+    bool var (int *a, int n, int t){
+    int j=n-2;
+    while (j!=-1&&a[j]>=a[j+1]) j--;
+    if (j==-1) return false;
+    int k=n-1;
+    while (a[j]>=a[k])k--;
+    swap(a,j,k);
+    int q=j+1; int r=n-1;
+    while (q<r)
+        swap(a,q++,r--);
+    return true;
+    }
+    void print (int *a, int n){
+    for (int i=0;i<n;i++)
+        cout <<a[i];
+        cout<<endl;
+    }
+bool poisk (int*a, int*b,int n){
+for (int i=0; i<n; i++)
+    if (a[i]!=b[i]) return false;
+    return true;
+
+}
+ int main()
+{
+    setlocale (LC_ALL,"rus");
+    int n,m;
+    cout<<"количество элементов:";
+    cin>>n;
+    cout<<"количество повторов:";
+    cin>>m;
+    double t=0; //время на операции
+
+     //int nomer;
+    for (int p=0; p<m; p++){
+    int *a=new int[n];
+     for (int i=0;i<n; i++) a[i]=i;
+    int*b=new int[n];
+    cout<<"введите перестановку:"<<endl;
+    for (int i=0; i<n; i++) cin>>b[i];
+    //nomer=1;
+    t++;
+    if  (poisk(a,b,n))
+         continue;
+     while(var(a,n,t)){
+            t++;
+            if (poisk(a,b,n)) break;
+            //nomer++;
+     }
+     //cout<<nomer<<endl; номер текущей перестановки
+     cout<<t<<endl; //текущее время поиска
+
+        delete [] b;
+        delete []a;
+     }
+     t=t/m;
+     cout<<t;
+     cin.get(); cin.get();
+    return 0;
+}
