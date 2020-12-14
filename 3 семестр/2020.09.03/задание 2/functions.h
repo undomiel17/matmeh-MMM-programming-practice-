3f3b6af6db0b2942d5916ab349ad22a151e58d3d
undomiel17 <55258395+undomiel17@users.noreply.github.com>
2020-12-14 22:24:43 +0300

    Add files via upload

---
+++ 3 семестр/2020.09.03/задание 2/functions.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <cmath>
+#include <ctime>
+#include <cstdlib>
+template <typename T>
+  class raid;
---
+++ 3 семестр/2020.09.03/задание 2/main.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <cmath>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+template <typename T>
+  class raid {
+private:
+   int number;
+   int length;
+   T ** buff;
+public:
+raid(int k, int n){
+number=k+1; //доп буфер для контр сумм
+length=n;
+T**arr=new T*[number];
+   for (int i=0; i<number; i++){
+    arr[i]=new T[length];
+   }
+buff=arr;
+for (int j=0; j<length;j++)
+    buff[number-1][j]=0; //буфер мы считаем, а это сразу заполним, чтобы не попал мусор
+}
+~raid(){
+for (int i=0; i<number; i++){
+    delete [] buff[i];
+}
+delete [] buff;
+    }
+ void print (){
+if (number==0) {
+        cout<<"no data";
+}
+else
+for (int i=0; i<number; i++){
+    if (i!=number-1) cout<<"disk"<<i+1<<":"<<endl;
+    else cout<<"control sum:"<<endl;
+  for (int j=0; j<length; j++){
+    cout<<"  "<<buff[i][j]<<" ";
+  }
+  cout<<endl;
+ }
+}
+void read (){
+    T x;
+    int i=0;
+    int j=0;
+cout<<"data:"<<endl;
+for (int step=0; step< (number-1)*length; step++ ){
+    cin>>x;
+    buff[i][j]=x;
+    j++;
+    if ((step+1)%length==0){
+        i++; //переходим на следующий диск
+        j=0;
+    }
+  }
+}
+void control(){
+for (int j=0; j<length;j++) {
+    for (int i=0;i<number-1; i++) //исключающее или равносильно остатку от деления на 2 суммы элементов
+      buff[number-1][j]+=buff[i][j];
+    buff[number-1][j]=buff[number-1][j]%2;
+    }
+ }
+void loss (int mistake ){
+for (int i=0; i<length;i++)
+    buff[mistake][i]=-1;
+}
+void recovery (int mistake){
+    for (int j=0; j<length;j++) {
+    for (int i=0;i<number; i++){
+      if (i!=mistake) buff[mistake][j]+=buff[i][j];
+    }
+    buff[mistake][j]=(buff[mistake][j]+1)%2; //восстанавливаем вычтенную единицу
+  }
+}
+
+};
+int main()
+{
+     srand(time(0));
+    int k,n;
+    cout<<"number of buffers:"<<endl;
+    cin>>k;
+    cout<<"length of buffers:"<<endl;
+    cin>>n;
+    raid <int> raid5(k,n);
+    raid5.read();
+    //raid5.print();
+    raid5.control();
+    raid5.print();
+    int mistake= rand()%n;
+    raid5.loss(mistake);
+    cout<<"data of disk "<<mistake+1<<" are lost"<<endl;
+    raid5.print();
+    raid5.recovery(mistake);
+    raid5.print();
+    return 0;
+}
