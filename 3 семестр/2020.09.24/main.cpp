e2f1212cec256c1f1e7456643f25b9250ceda849
undomiel17 <55258395+undomiel17@users.noreply.github.com>
2020-12-17 01:30:06 +0300

    Update main.cpp

--- 3 семестр/2020.09.24/main.cpp
+++ 3 семестр/2020.09.24/main.cpp
@@ -145,9 +145,9 @@ template <typename T1, typename T2>
 
     void swap(const T1& obj1, const T2& obj2)
     {
-//запоминает
+//Р·Р°РїРѕРјРёРЅР°РµС‚
         T1 tmp = obj1;
-//снимает константное значение с объектов. преобразует тип второго к типу первого
+//СЃРЅРёРјР°РµС‚ РєРѕРЅСЃС‚Р°РЅС‚РЅРѕРµ Р·РЅР°С‡РµРЅРёРµ, РїРѕСЃР»Рµ СЌС‚РѕРіРѕ РїСЂРёРІРѕРґРёС‚ С‚РёРїС‹
         const_cast<T1&> (obj1) = reinterpret_cast<T1&> (
                                     const_cast<T2&> (obj2));
         const_cast<T2&> (obj2) = reinterpret_cast<T2&> (
